17b3374e3bfd3d2a816ddae4625cae1f232c200b
Yujin-nKim <yujinkimvv@gmail.com>
2023-06-16 06:03:35 +0900

    보드 별 최신 업데이트 게시글 확인 코드 작성

--- ResquestOpenAPI.py
+++ ResquestOpenAPI.py
@@ -33,6 +33,19 @@ def call_openAPI(total_board_list, user_selected_board_id_list):
         except:
             print("API SERVER ERROR : NOT CONNECTED")
 
+        if response.status_code == 200:
+            data = response.json()
+            if data['OutBlock'][0]['MSG'] == 'N':
+                print("API SERVER DATA NULL")
+                continue
+            # api 응답 데이터의 최신 article_no
+            api_article_last_no = (int)(data['RESULT'][0]['article_no'])
+            # 보드 별로 업데이트 할 게시글이 있는지 확인 category_board_number의 last_board_no 이용
+            if api_article_last_no != db_article_last_no:
+                # 추후 category_board_number table update 위해 last_board_no update
+                board_item[2] = api_article_last_no
+                print(board_no, 'update')
+
 
         else:
             print("api SERVER ERROR")
